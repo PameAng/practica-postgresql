81fe51b0a6163a1ec9ba85dfb2e62b4a59f24edb
PameAng <pamelaangomas@hotmail.com>
2026-02-16 16:48:20 -0400

    Create transacciones.sql

---
+++ transacciones.sql
@@ -0,0 +1,59 @@
+-- CREACION DE TABLAS
+
+
+CREATE TABLE usuarios(
+	id SERIAL PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	apellido VARCHAR(100) NOT NULL,
+	email VARCHAR(200) UNIQUE
+);
+
+CREATE TABLE cuenta(
+	id SERIAL PRIMARY KEY,
+	usuario_id INTEGER NOT NULL,
+	monto DECIMAL(10, 2) CHECK(monto >= 0),
+	FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+);
+
+
+BEGIN;
+
+UPDATE cuenta SET monto = monto - 200 WHERE usuario_id = 1;
+
+UPDATE cuenta SET monto = monto + 200 WHERE usuario_id = 2;
+
+COMMIT;
+
+SELECT * FROM cuenta;
+
+
+
+BEGIN;
+
+UPDATE cuenta SET monto = monto - 5000 WHERE usuario_id = 1;
+
+ROLLBACK;
+
+SELECT * FROM cuenta;
+
+
+-- SESION 1
+
+BEGIN;
+
+UPDATE cuenta SET monto = monto - 100 WHERE usuario_id = 1;
+
+ROLLBACK;
+
+SELECT * FROM cuenta;
+
+
+-- SESION 2
+
+BEGIN;
+
+UPDATE cuenta SET monto = monto - 300 WHERE usuario_id = 1;
+
+COMMIT;
+
+SELECT * FROM cuenta;
